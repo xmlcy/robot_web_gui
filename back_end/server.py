b69d97d69bcbe8bd81f95cba970a92602b7c327d
chase <lcy5656@qq.com>
2022-11-11 16:42:26 +0800

    增加 websockets demo

---
+++ back_end/server.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+
+async def echo(websocket, path):
+    #fetch msg
+    async for message in websocket:
+        print("got a message:{}".format(message))
+        await websocket.send(message)
+
+async def main():
+    # start a websocket server
+    async with websockets.serve(echo, "localhost", 8080):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
