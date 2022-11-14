3824fc2ca00d04de1e45f12e4291e1744117a537
chase <lcy5656@qq.com>
2022-11-14 17:03:52 +0800

    增加ros2功能包和相关说明

--- README.md
+++ README.md
@@ -29,3 +29,29 @@ python server.py
 2. 使用浏览器打开 front_end/index.html
 
 3. 选择表情演示 点击表情菜单中的按钮即可在服务端看到消息
+
+
+## ROS2 功能包
+
+由于websockets中使用asyncio，还没研究明白，所以暂时只能单方向接收web数据，通过ros2发布
+
+### 功能包使用
+
+1. ros2_ws文件夹下编译运行节点
+
+```shell
+cd ros2_ws/
+colcon build
+source install/local_setup.bash
+ros2 run ros2_bridge_py bridge_websocket
+```
+
+2. 另一个终端执行，可以看到该节点发布的消息
+
+```shell
+ros2 topic echo /action
+```
+
+3. 使用浏览器打开 front_end/index.html
+
+4. 选择表情演示 点击表情菜单中的按钮即可在终端看到消息
--- ros2_ws/src/ros2_bridge_py/ros2_bridge_py/bridge_websocket.py
+++ ros2_ws/src/ros2_bridge_py/ros2_bridge_py/bridge_websocket.py
@@ -1,6 +1,45 @@
-def main():
-    print('Hi from ros2_bridge_py.')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import asyncio
+import websockets
 
 
-if __name__ == '__main__':
-    main()
+class WebsocketNode(Node):
+    def __init__(self, name):
+        super().__init__(name)
+        self.pub = self.create_publisher(String, "action", 10) # 转发websocket消息
+
+
+# async def run_node():
+#     rclpy.init()
+#     node = WebsocketNode("web_node")
+#     rclpy.spin(node)
+#     node.destroy_node()
+#     rclpy.shutdown()
+
+rclpy.init()
+node = WebsocketNode("web_node")
+
+
+async def echo(websocket):
+    #fetch msg
+    async for message in websocket:
+        print("got a message:{}".format(message))
+        await websocket.send(message)
+        msg = String()
+        # print(type(msg.data))
+        # print(type(message))
+        msg.data = message
+        node.pub.publish(msg)
+
+
+async def main(args=None):
+    # start a websocket server
+    async with websockets.serve(echo, "localhost", 8080):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
